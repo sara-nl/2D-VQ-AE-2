e659afe1dbc0bd2ebe5c7aa2cec18e358bc48e56
Robert Jan Schlimbach <robertjan.schlimbach@gmail.com>
2021-11-22 14:56:05 +0100

    upgrade to 2021 stack: ASAP2.0/python3.9

--- load-asap.sh
+++ load-asap.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-module load 2020
-module load Miniconda3/4.7.12.1
+module load 2021
+module load Miniconda3/4.9.2
 
-module load ASAP/8c9a8fb-fosscuda-2020a
+module load ASAP/2.0-foss-2021a-CUDA-11.3.1
 
 source deactivate
 source deactivate
 source activate 2D-VQ-AE-2
 
-export PYTHONPATH=$PYTHONPATH:~/.conda/envs/2D-VQ-AE-2/lib/python3.8/site-packages/
+export PYTHONPATH=$PYTHONPATH:~/.conda/envs/2D-VQ-AE-2/lib/python3.9/site-packages/
