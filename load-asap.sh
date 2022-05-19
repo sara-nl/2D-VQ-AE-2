4cb6da98a3d57c5a341c8a0a5abb43b21045415a
Robert Jan Schlimbach <robertjan.schlimbach@gmail.com>
2022-05-19 15:40:10 +0200

    update dependencies

--- load-asap.sh
+++ load-asap.sh
@@ -5,3 +5,5 @@ module load ASAP/2.0-foss-2021a-CUDA-11.3.1
 module load Python/3.9.5-GCCcore-10.3.0
 
 eval "$(pdm --pep582)"
+
+module unload cuDNN # module cuDNN interferes with pdm installed cuDNN
